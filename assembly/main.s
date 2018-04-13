df649161c7b41170173933ad30a6d91527c42344
Chris Bendel <chrissbendel@gmail.com>
2018-04-13 16:59:01 -0400

    getting file contents

--- assembly/main.s
+++ assembly/main.s
@@ -1,45 +1,41 @@
-#Chris Bendel
-#Assignment 12
+
+.bss
+.lcomm buffer, 4096
 
 .data
-output:    .ascii "Computer Architecture\n"
-len: .quad (. - output)
 file_name: .asciz "/Users/chris/Desktop/test.txt"
 copy_name: .asciz "/Users/chris/Desktop/testcopy.txt"
-test: .ascii ""
-//copy_len: .quad 1024
-copy_text: .ascii ""
-copy_len: .quad 12
+
 file_handle: .long 0
 copy_file_handle: .long 0
+
+copy_text: .quad 4096
+copy_len: .quad 0
+length: .quad 4096
 
 .text
 .globl _main
 _main:
 
-//movq $0x2000005, %rax        # system open (5)
-//leaq file_name(%rip), %rdi   # path
-//movq $0x0000, %rsi           # flags (0000=RD_ONLY)
-//movq $0444, %rdx             # mode (permissions)
-//syscall
-//movq %rax, file_handle(%rip)
-
-# Opens file to copy to
-//movq $0x2000005, %rax        # system open (5)
-//leaq file_copy_name(%rip), %rdi   # path
-//movq $0x0201, %rsi           # flags (02=O_CREAT, 01=O_WRONLY)
-//movq $0666, %rdx             # mode (permissions)
-//syscall
-//movq %rax, copy_file_handle(%rip)
-
-# Copy old file to new file
-movq $0x2000227, %rax        # system copyfile (5)
-leaq file_name(%rip), %rdi   # First path
-leaq copy_name(%rip), %rsi   # Second path
-movq $0666, %rdx             # mode (permissions)
-movq $0x0201, %rcx           # flags (02=O_CREAT, 01=O_WRONLY)
+movq $0x2000005, %rax        # system open (5)
+leaq file_name(%rip), %rdi   # path
+movq $0x0000, %rsi           # flags (02=O_CREAT, 01=O_WRONLY)
+movq $0444, %rdx             # mode (permissions)
 syscall
-//movq %rax, copy_file_handle(%rip)
+movq %rax, file_handle(%rip)
+
+movq $0x2000003, %rax          # system read (3)
+movq file_handle(%rip), %rdi   # file handle
+leaq buffer(%rip), %rsi
+movq length(%rip), %rdx
+syscall
+movq %rax, copy_len(%rip)
+
+movq $0x2000004, %rax        # system write (4)
+movq $1, %rdi                # file handle STDOUT (1)
+leaq buffer(%rip), %rsi      # address
+movq copy_len(%rip), %rdx         # length
+syscall
 
 //movq $0x2000003, %rax        # system read (3)
 //leaq file_handle(%rip), %rdi   # path
@@ -50,11 +46,19 @@ syscall
 //syscall
 //movq %rax, copy_text(%rip)
 
-//movq $0x2000004, %rax        # system write (4)
-//movq $1, %rdi                # file handle STDOUT (1)
-//leaq copy_text(%rip), %rsi      # address
-//movq copy_len(%rip), %rdx         # length
+
+# Copy old file to new file
+//movq $0x20000E3, %rax        # system copyfile (227)
+//leaq file_name(%rip), %rdi   # First path
+//leaq copy_name(%rip), %rsi   # Second path
+//movq $0666, %rdx             # mode (permissions)
+//movq $0x0201, %rcx           # flags (02=O_CREAT, 01=O_WRONLY)
 //syscall
+//movq %rax, copy_file_handle(%rip)
+
+
+
+
 //
 //movq $0x2000004, %rax        # system write (4)
 //movq file_handle(%rip), %rdi # file handle
